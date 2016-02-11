a5d389c47e9b714f0409a870c1218fff4660f427
Ritesh Kumar <rkritesh078@gmail.com>
2016-02-12 02:06:58 +0530

    feat(css-modules): added support for css modules loader with sass integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,55 @@
 'use strict';
 
-var webpack = require('webpack');
-var path = require('path');
+var webpack           = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path              = require('path');
 
 var plugins = [
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.optimize.OccurenceOrderPlugin(),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  })
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
 ];
 
 if (process.env.NODE_ENV === 'production') {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        screw_ie8: true,
-        warnings: false
-      }
-    })
-  );
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                screw_ie8: true,
+                warnings : false
+            }
+        })
+    );
 }
 
 module.exports = {
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['babel-loader'],
-      exclude: /node_modules/,
-      include: path.join(__dirname, 'src')
-    }]
-  },
-  entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client',
-    './src/index.js'
-  ],
-  devtool: 'cheap-module-eval-source-map',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: plugins,
-  resolve: {
-    extensions: ['', '.js']
-  }
+    module : {
+        loaders: [{
+            test   : /\.jsx?$/,
+            loaders: ['babel-loader'],
+            exclude: /node_modules/,
+            include: path.join(__dirname, 'components')
+        }, {
+            test  : /\.scss$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass')
+        }]
+    },
+    entry  : [
+        'eventsource-polyfill', // necessary for hot reloading with IE
+        'webpack-hot-middleware/client',
+        './components/index.js'
+    ],
+    devtool: 'cheap-module-eval-source-map',
+    output : {
+        path         : path.join(__dirname, 'dist'),
+        filename     : 'bundle.js',
+        publicPath   : '/static/',
+        libraryTarget: 'umd'
+    },
+    plugins: plugins,
+    resolve: {
+        extensions: ['', '.js']
+    }
 };
